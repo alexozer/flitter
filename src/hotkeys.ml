f930b81515c8d6c87335ae350aef4c3237a2548a
Alex Ozer <alexozer@users.noreply.github.com>
2018-09-30 23:10:02 -0400

    Python hotkeys server exits gracefully when timer exits

--- src/hotkeys.ml
+++ src/hotkeys.ml
@@ -5,20 +5,32 @@ type t = keypress Lwt_stream.t
 
 let python_detect_keys = {|
 import time
+import sys
 from pynput import keyboard
 
+
 def on_press(key):
-    t = time.time()
     try:
-        # Alphanumeric key pressed
-        print('{} {}'.format(t, key.char), flush=True)
-    except AttributeError:
-        # Special key pressed
-        key_name = str(key)[4:] # Strip "Key."
-        print('{} {}'.format(t, key_name), flush=True)
+        t = time.time()
+        try:
+            # Alphanumeric key pressed
+            print('{} {}'.format(t, key.char), flush=True)
+        except AttributeError:
+            # Special key pressed
+            key_name = str(key)[4:] # Strip "Key."
+            print('{} {}'.format(t, key_name), flush=True)
+    except:
+        sys.exit(0)
 
 # Collect events until released
 with keyboard.Listener(on_press=on_press) as listener:
+    try:
+        while True:
+            t = time.time()
+            print('{} heartbeat'.format(t))
+            time.sleep(1)
+    except:
+        sys.exit(0)
     listener.join()
 |}
 
