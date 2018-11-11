e8e9838e3a258d5f2fcc8620eee0603f84691926
Alex Ozer <alexozer@users.noreply.github.com>
2018-11-11 03:54:50 -0500

    Make small example have less splits

--- examples/splits-small.scm
+++ examples/splits-small.scm
@@ -9,11 +9,6 @@
     Blue
     Brown
     Pink
-    White
-    Teal
-    Orange
-    Gold
-    Black
     ))
 
  (personal_best 
@@ -26,11 +21,6 @@
        ((title Blue) (time 6) (is_gold false))
        ((title Brown) (time 8) (is_gold false))
        ((title Pink) (time 10) (is_gold false))
-       ((title White) (time 12) (is_gold false))
-       ((title Teal) (time 14) (is_gold false))
-       ((title Orange) (time 16) (is_gold false))
-       ((title Gold) (time 18) (is_gold false))
-       ((title Black) (time 20) (is_gold false))
        ))
     )
    )
@@ -41,10 +31,5 @@
      ((title Blue) (duration 1))
      ((title Brown) (duration 1))
      ((title Pink) (duration 1))
-     ((title White) (duration 1))
-     ((title Teal) (duration 1))
-     ((title Orange) (duration 1))
-     ((title Gold) (duration 1))
-     ((title Black) (duration 1))
      )
    ))
