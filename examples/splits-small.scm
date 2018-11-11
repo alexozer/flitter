da43d48eeb8724ac977ddb545622d455bc7a434c
Alex Ozer <alexozer@users.noreply.github.com>
2018-11-10 22:41:08 -0500

    Add better demo

---
+++ examples/splits-small.scm
@@ -0,0 +1,50 @@
+((title "Super Monkey Ball 2: Monkeyed Ball")
+ (category "Story Mode All Levels")
+ (attempts 3315)
+ (completed 30)
+ (split_names 
+   (
+    Green
+    Apricot
+    Blue
+    Brown
+    Pink
+    White
+    Teal
+    Orange
+    Gold
+    Black
+    ))
+
+ (personal_best 
+   (
+    (attempt 3300)
+    (splits 
+      (
+       ((title Green) (time 2) (is_gold false))
+       ((title Apricot) (time 4) (is_gold false))
+       ((title Blue) (time 6) (is_gold false))
+       ((title Brown) (time 8) (is_gold false))
+       ((title Pink) (time 10) (is_gold false))
+       ((title White) (time 12) (is_gold false))
+       ((title Teal) (time 14) (is_gold false))
+       ((title Orange) (time 16) (is_gold false))
+       ((title Gold) (time 18) (is_gold false))
+       ((title Black) (time 20) (is_gold false))
+       ))
+    )
+   )
+
+   (golds (
+     ((title Green) (duration 1))
+     ((title Apricot) (duration 1))
+     ((title Blue) (duration 1))
+     ((title Brown) (duration 1))
+     ((title Pink) (duration 1))
+     ((title White) (duration 1))
+     ((title Teal) (duration 1))
+     ((title Orange) (duration 1))
+     ((title Gold) (duration 1))
+     ((title Black) (duration 1))
+     )
+   ))
--- src/demo.ml
+++ src/demo.ml
@@ -2,11 +2,18 @@ open Timer_types
 
 let timer =
   let pb = Some {
-      attempt = 2500;
+      attempt = 2000;
       splits = [|
-        {title = "Green"; time = Some 3000; is_gold = false};
-        {title = "Apricot"; time = Some 5000; is_gold = false};
-        {title = "Blue"; time = Some 8000; is_gold = false};
+        {title = "Green"; time = Some 2; is_gold = false};
+        {title = "Apricot"; time = Some 4; is_gold = false};
+        {title = "Blue"; time = Some 6; is_gold = false};
+        {title = "Brown"; time = Some 8; is_gold = false};
+        {title = "Pink"; time = Some 10; is_gold = false};
+        {title = "White"; time = Some 12; is_gold = false};
+        {title = "Teal"; time = Some 14; is_gold = false};
+        {title = "Orange"; time = Some 16; is_gold = false};
+        {title = "Gold"; time = Some 18; is_gold = false};
+        {title = "Black"; time = Some 20; is_gold = false};
       |];
     } in
 
@@ -20,15 +27,29 @@ let timer =
       "Green";
       "Apricot";
       "Blue";
+      "Brown";
+      "Pink";
+      "White";
+      "Teal";
+      "Orange";
+      "Gold";
+      "Black";
     |];
 
     golds = [|
-      {title = "Green"; duration = Some 2000};
-      {title = "Apricot"; duration = Some 2000};
-      {title = "Blue"; duration = Some 2000};
+      {title = "Green"; duration = Some 1};
+      {title = "Apricot"; duration = Some 1};
+      {title = "Blue"; duration = Some 1};
+      {title = "Brown"; duration = Some 1};
+      {title = "Pink"; duration = Some 1};
+      {title = "White"; duration = Some 1};
+      {title = "Teal"; duration = Some 1};
+      {title = "Orange"; duration = Some 1};
+      {title = "Gold"; duration = Some 1};
+      {title = "Black"; duration = Some 1};
     |];
 
     comparison = pb;
     history = [];
-    state = Timing ([|Some 1500|], Unix.gettimeofday ())
+    state = Idle;
   }
