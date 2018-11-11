a4816da5af2e22ac90c7f261372c52667e43c04b
Alex Ozer <alexozer@users.noreply.github.com>
2018-11-11 16:11:48 -0500

    Remove demo
    You can just launch an example splits file for a demo

--- src/flitter.ml
+++ src/flitter.ml
@@ -1,10 +1,7 @@
 let usage = 
   "Usage:\n" ^
-  "\n" ^
-  "flitter -d\n" ^
-  "Open demo splits.\n" ^
-  "\n" ^
-  "flitter -o <splits_path>\n" ^
+
+  "flitter <splits_path>\n" ^
   "Open the splits file pointed to by `splits_path`.\n"
 
 let run_event_loop timer =
@@ -13,11 +10,8 @@ let run_event_loop timer =
 
 let run () =
   match Sys.argv with
-  | [|_; "-o"; path|] ->
+  | [|_; path|] ->
     let timer = Loadsave.load path in
     Lwt_main.run (run_event_loop timer)
-
-  | [|_; "-d"|] ->
-    Lwt_main.run (run_event_loop Demo.timer)
 
   | _ -> print_string usage
