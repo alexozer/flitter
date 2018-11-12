ee174d83913b302593200fc755af0971349f727a
Alex Ozer <alexozer@users.noreply.github.com>
2018-11-12 02:00:15 -0500

    Format example splits

--- examples/splits.scm
+++ examples/splits.scm
@@ -1,69 +1,42 @@
-((title "Super Monkey Ball 2: Monkeyed Ball")
- (category "Story Mode All Levels")
- (attempts 3315)
+((title     "Super Monkey Ball 2: Monkeyed Ball")
+ (category  "Story Mode All Levels")
+ (attempts  3316)
  (completed 30)
- (split_names 
-   (
-    Green
-    Apricot
-    Blue
-    Brown
-    Pink
-    White
-    Teal
-    Orange
-    Gold
-    Black
-    ))
-
- (personal_best 
-   (
-    (attempt 3300)
-    (splits 
-      (
-       ((title Green) (time 2:13.452) (is_gold false))
-       ((title Apricot) (time 4:38.674) (is_gold false))
-       ((title Blue) (time 6:40.914) (is_gold false))
-       ((title Brown) (time 9:01.587) (is_gold false))
-       ((title Pink) (time 11:10.919) (is_gold false))
-       ((title White) (time 13:48.715) (is_gold false))
-       ((title Teal) (time 16:23.567) (is_gold false))
-       ((title Orange) (time 18:52.029) (is_gold false))
-       ((title Gold) (time 21:30.100) (is_gold false))
-       ((title Black) (time 25:32.917) (is_gold false))
-       ))
-    )
-   )
-
- (world_record 
-   (
-    (attempt 0)
-    (splits 
-      (
-       ((title Green) (time 2:12) (is_gold false))
-       ((title Apricot) (time 4:27) (is_gold false))
-       ((title Blue) (time 6:28) (is_gold false))
-       ((title Brown) (time 8:46) (is_gold false))
-       ((title Pink) (time 10:59) (is_gold false))
-       ((title White) (time 13:36) (is_gold false))
-       ((title Teal) (time 16:00) (is_gold false))
-       ((title Orange) (time 18:17) (is_gold false))
-       ((title Gold) (time 20:31) (is_gold false))
-       ((title Black) (time 24:16) (is_gold false))
-       ))
-    )
-   )
-
-   (golds (
-     ((title Green) (duration 2:10.664))
-     ((title Apricot) (duration 2:19.000))
-     ((title Blue) (duration 2:02.240))
-     ((title Brown) (duration 2:16.000))
-     ((title Pink) (duration 2:09.331))
-     ((title White) (duration 2:24.000))
-     ((title Teal) (duration 2:34.852))
-     ((title Orange) (duration 2:28.461))
-     ((title Gold) (duration 2:29.000))
-     ((title Black) (duration 4:02.817))
-     )
-   )
+ (split_names (Green Apricot Blue Brown Pink White Teal Orange Gold Black))
+ (golds (
+   ((title Green)   (duration 2:11.832))
+   ((title Apricot) (duration 2:19.000))
+   ((title Blue)    (duration 2:02.042))
+   ((title Brown)   (duration 2:16.000))
+   ((title Pink)    (duration 2:09.133))
+   ((title White)   (duration 2:24.000))
+   ((title Teal)    (duration 2:34.258))
+   ((title Orange)  (duration 2:28.164))
+   ((title Gold)    (duration 2:29.000))
+   ((title Black)   (duration 4:02.718))))
+ (personal_best (
+   (attempt 3300)
+   (splits (
+     ((title Green)   (time 2:13.254)  (is_gold false))
+     ((title Apricot) (time 4:38.476)  (is_gold false))
+     ((title Blue)    (time 6:40.419)  (is_gold false))
+     ((title Brown)   (time 9:01.785)  (is_gold false))
+     ((title Pink)    (time 11:10.919) (is_gold false))
+     ((title White)   (time 13:48.517) (is_gold false))
+     ((title Teal)    (time 16:23.765) (is_gold false))
+     ((title Orange)  (time 18:52.920) (is_gold false))
+     ((title Gold)    (time 21:30.001) (is_gold false))
+     ((title Black)   (time 25:32.719) (is_gold false))))))
+ (world_record (
+   (attempt 0)
+   (splits (
+     ((title Green)   (time 2:12.000)  (is_gold false))
+     ((title Apricot) (time 4:27.000)  (is_gold false))
+     ((title Blue)    (time 6:28.000)  (is_gold false))
+     ((title Brown)   (time 8:46.000)  (is_gold false))
+     ((title Pink)    (time 10:59.000) (is_gold false))
+     ((title White)   (time 13:36.000) (is_gold false))
+     ((title Teal)    (time 16:00.000) (is_gold false))
+     ((title Orange)  (time 18:17.000) (is_gold false))
+     ((title Gold)    (time 20:31.000) (is_gold false))
+     ((title Black)   (time 24:16.000) (is_gold false)))))))
