95a3196a52a9dd66b04287cca986d750f57a60b8
Alex Ozer <alexozer@users.noreply.github.com>
2024-09-04 17:49:37 -0400

    Test rendering multiple rows/cols

--- src/timer.rs
+++ src/timer.rs
@@ -30,14 +30,17 @@ impl Timer {
         if read_chars()?.contains(&'q') {
             return Ok(false);
         }
-        let line1 = Image::new("Hello, world!", 20, TextAlign::Left)
-            .attr(style::Attribute::Bold)
-            .build();
-        let line2 = Image::new("Goodbye, world!", 20, TextAlign::Right)
-            .fg_color(style::Color::Red)
-            .attr(style::Attribute::Underlined)
-            .build();
-        let vert = line1.vert(line2);
+
+        let row1: Vec<Block> = (0..3)
+            .map(|i| Image::new(&format!("Cell {i}"), 10, TextAlign::Left).build())
+            .collect();
+        let row2: Vec<Block> = (3..6)
+            .map(|i| Image::new(&format!("Cell {i}"), 10, TextAlign::Left).build())
+            .collect();
+        let row1 = Block::hcat(row1);
+        let row2 = Block::hcat(row2);
+        let vert = row1.vert(row2);
+
         self.renderer.render(&vert)?;
         Ok(true)
     }
